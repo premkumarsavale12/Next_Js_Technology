62f952bb42081b7c65ef1d8d785330c8aa9bd68b
Premkumar Savale <premsavle112@gmail.com>
2025-12-30 16:46:43 +0530

    Added Routing Code

--- app/page.js
+++ app/page.js
@@ -1,39 +1,13 @@
 
-"use client"
-import { useState } from "react"
-
-export default function Home() {
-
-  const [data, setData] = useState(0);
-
-  const Incre = () => {
-
-    setData(data + 1);
-
-  }
-
-
-  const Decre = () => {
-    if (data == 0) {
-      alert(" 0 is not be descread");
-
-
-    }
-    else {
-      setData(data - 1);
-
-    }
-  }
+const page = () => {
 
   return (
 
     <>
-
-      <h1> {data}</h1>
-      <button onClick={Incre}> Incerement </button>
-      <button onClick={Decre}> Decrement </button>
-
+      <h1> Welcome to Next Js Technology.... </h1>
 
     </>
   )
 }
+
+export default page
