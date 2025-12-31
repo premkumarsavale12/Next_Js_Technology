f512f7a503de07fc2c24de077e1bbbc44d1ccdcc
Premkumar Savale <premsavle112@gmail.com>
2025-12-31 14:43:55 +0530

    Added Code Layout file

---
+++ app/about/layout.js
@@ -0,0 +1,11 @@
+export default function Layout({ children }) {
+    return (
+
+        <>
+
+            <h1> This is Common Layout For About Page </h1>
+
+            {children}
+        </>
+    )
+}
---
+++ app/login/layout.js
@@ -0,0 +1,14 @@
+export default function Layout({ children }) {
+    return (
+
+        <>
+
+            <h1> This is Common Layout Only Login Page </h1> 
+
+
+            {children}
+
+        </>
+    )
+}
+
