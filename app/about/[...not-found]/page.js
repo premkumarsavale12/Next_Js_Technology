2ea8e0255d42d6f3382834a69f8ec5bbc824b9c4
Premkumar Savale <premsavle112@gmail.com>
2025-12-31 15:53:40 +0530

    added layout & not found page code

---
+++ app/about/[...not-found]/page.js
@@ -0,0 +1,12 @@
+
+export default function AboutPage() {
+
+    return (
+
+        <>
+            <h1> This is About Page for not Found </h1>
+        </>
+    )
+}
+
+
---
+++ app/not-found.js
@@ -0,0 +1,9 @@
+export default function Not_NotFound() {
+    return (
+
+        <>
+
+            <h1> This is Not Found Page .....</h1>
+        </>
+    )
+}
